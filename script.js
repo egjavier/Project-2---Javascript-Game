311e38008440b38660c9ee8cf59811fdf212ab19
egjavier <yunisjavier002@gmail.com>
2023-09-28 00:05:12 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const gameContainer = document.querySelector('#gameContainer');
+
+gameContainer.children[0].addEventListener('click', flip);
+
+function flip(){
+  console.log('clicked')
+};
+function declareImage(){};
+function updateScore(){};
